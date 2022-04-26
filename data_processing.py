c535cf1119ebd15446e3e52c2266fe84085422bb
divakar1983b <divakar1983.b@hotmail.com>
2022-04-26 23:16:55 +0530

    test2

--- data_processing.py
+++ data_processing.py
@@ -1,10 +1,3 @@
 print("test python data processing code")
-<<<<<<< HEAD
 print("dp second line")
-=======
-print ("this is the second line")
-print("this is the third line")
-print("this is the fourth line")
-print("this is the fifth line")
-print("this is the sixth line")
->>>>>>> 6214424c6acf192ff51d226feb32af3b32e48518
+
